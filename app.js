f881447c2f422c7d0acf3c6295af62e806e9b63a
mbarut1 <vtme996@gmail.com>
2020-03-06 23:00:20 -0500

    Add more columns to the db and moved some code around

--- app.js
+++ app.js
@@ -18,14 +18,20 @@ app.get('/create-test-table', (req, res)=>{
     // SQL query string that will create a new test table
     let SQL = `CREATE TABLE test(
                     id INT AUTO_INCREMENT NOT NULL, 
-                    pubDate VARCHAR(255) NOT NULL,
+                    pubDate DATETIME NOT NULL,
                     title VARCHAR(255) NOT NULL,
+                    link VARCHAR(2048) NOT NULL,
+                    source VARCHAR(64),
                     PRIMARY KEY(id)
                 )`;
     // Taking the query string from above and running it on the database to actually create the table
     // and also handling the response
-    queryDB(SQL, (msg) =>{
-        res.send(msg);
+    queryDB(SQL, (err) =>{
+        if(err){
+            res.send(err.sqlMessage);
+        }else{
+            res.send("Table Created...");
+        }
         res.end();
     });
 });
@@ -45,16 +51,15 @@ app.get('/api/search-term/:searchTerm', (req, res) =>{
         .then(res => res.text()) // Converting the XML response into TEXT
         .then(data =>{
 
-            // Return a response
-            res.send(data); // Sending that data as a response to the frontend.
-            res.end(); // Ending the response
-
-            // Parsing XML text into json, then extracting the pubDate and title
-            // using the a helper function. Using a callback function to query the database
-            // and INSERT the rows into the database table.
+            // Parsing XML text into json, then extracting the desired data using the a parseXML2JSON function.
+            // Using the callback function parameter to query the database and INSERT the rows into the database table.
             parseXML2JSON(data, (values) =>{
+                // Return a response
+                res.send(values); // Sending that data as a response to the frontend.
+                res.end(); // Ending the response
+
                 // Creating the SQL query that will insert multiple rows into our database
-                let SQL = "INSERT INTO test (pubDate, title) VALUES ?";
+                let SQL = "INSERT INTO test (pubDate, title, link, source) VALUES ?";
 
                 //Connecting to the database to run the query and make the actual changes to the database
                 queryDB(SQL, [values]);
