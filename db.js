af796a2f132f30ffdbe3294c3f71b635b6f40719
mbarut1 <vtme996@gmail.com>
2020-03-06 22:58:50 -0500

    Moved some code around

--- db.js
+++ db.js
@@ -3,39 +3,33 @@
  */
 const mysql = require('mysql');
 
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'test'
+});
+
+db.connect((err) => {
+    // in case of throw error
+    if(err) throw err;
+
+    // Otherwise log success message
+    console.log("MySql Connected...");
+});
+
 function queryDB(SQL, values = null, callback) {
-
-    const db = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : 'test'
-    });
-
-    db.connect((err) => {
-        // in case of throw error
-        if(err) throw err;
-
-        // Otherwise log success message
-        console.log("MySql Connected...");
-
-    });
 
     db.query(SQL, values, (err, result) => {
         if(err) {
             console.log(err);
             if(callback){
-                callback(err.message);
+                callback(err);
             }
         }else{
             console.log(result);
-            if(callback){
-                callback("Table Created...");
-            }
         }
     });
-
-    db.end();
 }
 
 module.exports = queryDB;
